e2b6d010f9290b5b002b5376fc4ea270f956e722
MasoomaSuleman <masooma20suleman@gmail.com>
2025-09-30 13:52:30 -0400

    added memory agent

--- ai_companion/sub_agents/memory_agent/agent.py
+++ ai_companion/sub_agents/memory_agent/agent.py
@@ -1,8 +1,23 @@
 from google.adk.agents import Agent
+import requests
+
+def fetch_relevant_memory(user_message = str):
+    """Retrieves the relevant memories based on latest user message
+
+    Args:
+        user_message (str): Latest user message
+
+    Returns:
+        relevant memory: list of relevant memory
+    """
+    print(user_message)
+    return ["",""]
+
 
 memoryAgent = Agent(
     name="memoryAgent",
     model="gemini-2.0-flash",
-    description="Memory agent that retrieves and manages conversation history and user preferences",
+    description="Memory agent that retrieves relevant conversation history",
     instruction="You are a memory agent responsible for retrieving relevant conversation history and user preferences to provide context for the conversation. Focus on remembering personal details, preferences, and important information shared by the user.",
+    tools = [fetch_relevant_memory]
 )
---
+++ ai_companion/sub_agents/memory_agent/prompt.py
@@ -0,0 +1,4 @@
+RETREIVE_RELEVANT_MEMORY = """
+You are a memory agent responsible for retrieving relevant conversation history for the latest user message using the fetch_relevant_memory tool. 
+Summarize these memories in 4-5 sentences and if no relevant memory is fetched then respond with saying that no relevant memory was found.
+"""
